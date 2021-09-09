6aa49da5cf41d3858f0be5b35805efde6d40735a
St0rmbr1ng3r <74727838+St0rmbr1ng3r@users.noreply.github.com>
2021-09-09 20:29:57 -0300

    test2

--- IRA_Server/wsgi.py
+++ IRA_Server/wsgi.py
@@ -21,6 +21,6 @@ import os
 import sys
 sys.path.append('/opt/bitnami/Apps/iraconsole/IRA_Server')
 os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/Apps/iraconsole/IRA_Server/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "/opt/bitnami/Apps/iraconsole/IRA_Server/IRA_Server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IRA_Server.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
