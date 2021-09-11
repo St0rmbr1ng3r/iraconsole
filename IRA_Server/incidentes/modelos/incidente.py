b3391ea899598615b9e8f8c9c89c26bf78fee035
St0rmbr1ng3r <74727838+St0rmbr1ng3r@users.noreply.github.com>
2021-09-11 17:28:12 -0300

    test

--- IRA_Server/incidentes/modelos/incidente.py
+++ IRA_Server/incidentes/modelos/incidente.py
@@ -217,22 +217,12 @@ class NuevoIncidente(models.Model):
         act_afectados = formulario['act_afectados']
         id_impacto = formulario['id_impacto']
         id_urgencia = formulario['id_urgencia']
-        id_severidad = formulario['id_severidad']
-
-        print(id_etapa,
-				id_tipo,
-				id_origen,
-				desc_inc,
-				cli_afectados,
-				prov_involucrado,
-				act_afectados,
-				id_impacto,
-				id_urgencia,
-				id_severidad)
-
+        #id_severidad = formulario['id_severidad']
+        id_severidad = '2'
         args=[id_etapa, id_tipo, id_origen, desc_inc, cli_afectados, prov_involucrado,	act_afectados,	id_impacto,	id_urgencia, id_severidad]
 
         print(args)
+
         cursorGuardarIncidente=connection.cursor()
         cursorGuardarIncidente.execute('call GuardarNuevoIncidente()',args)
         resGuardarIncidente=cursorGuardarIncidente.fetchall()
