af5700cacb1f333bf4fe645dff0695f35151ed4d
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-12 03:27:06 -0300

    getst

--- IRA_Server/wsgi.py
+++ IRA_Server/wsgi.py
@@ -6,21 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-'''
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IRA_Server.settings')
 
-application = get_wsgi_application()
-'''
-
-import os
-import sys
-import incidentes
-sys.path.append('/opt/bitnami/Apps/iraconsole/')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/Apps/iraconsole/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
