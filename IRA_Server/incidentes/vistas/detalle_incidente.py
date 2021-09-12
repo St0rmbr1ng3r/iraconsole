c995a0bd7a49817339d199e520d7193a9dad4085
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-12 19:35:51 -0300

    test

--- IRA_Server/incidentes/vistas/detalle_incidente.py
+++ IRA_Server/incidentes/vistas/detalle_incidente.py
@@ -11,26 +11,28 @@ def cargar_incidente(request):
         di = DetalleIncidente()
         detalle = di.buscar_incidente(id_inc)
 
+        descripciones = []
+
         if detalle[0][6] == 1:
             print("No hubo Clientes Afectados")
-            detalle[0][6] = "No hubo Clientes Afectados"
+            descripciones.append("No hubo Clientes Afectados")
         else:
             print("Hay Clientes Afectados")
-            detalle[0][6] = "Hay Clientes Afectados"
+            descripciones.append("Hay Clientes Afectados")
 
         if detalle[0][7] == 1:
             print("No hubo Proveedores Involucrados")
-            detalle[0][7] = "No hubo Proveedores Involucrados"
+            descripciones.append("No hubo Proveedores Involucrados")
         else:
             print("Hay Proveedores Involucrados")
-            detalle[0][7] = "Hay Proveedores Involucrados"
+            descripciones.append("Hay Proveedores Involucrados")
 
         if detalle[0][8] == 1:
             print("Activos Afectados 0-25%")
-            detalle[0][8] = "Activos Afectados 0-25%"
+            descripciones.append("Activos Afectados 0-25%")
         else:
             print("Mayor a 25%")
-            detalle[0][8] = "Mayor a 25%"
+            descripciones.append("Mayor a 25%")
     
         contexto = {'detalle':detalle}
         return render(request, "detalle_incidente.html", contexto)
