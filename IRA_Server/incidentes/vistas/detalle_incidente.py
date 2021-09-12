9e06b7132a70d25098cd9a94849eb498faeae3fe
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-12 17:32:49 -0300

    test

--- IRA_Server/incidentes/vistas/detalle_incidente.py
+++ IRA_Server/incidentes/vistas/detalle_incidente.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from modelos.incidente import DetalleIncidente
 
 @login_required(login_url='login')
 def cargar_incidente(request):
@@ -10,5 +11,9 @@ def cargar_incidente(request):
         print(request)
         id_inc=request.GET.get('id_inc', '0')
         print("ID INCIDENTE: ",id_inc)
-        contexto = {}
+
+        di = DetalleIncidente()
+        detalle=di.buscar_incidente(id_inc)
+        print("DETALLE DE INC: ",detalle)
+        contexto = {detalle}
         return render(request, "detalle_incidente.html", contexto)
