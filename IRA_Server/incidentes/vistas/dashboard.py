699d598b08151ffb4c121f21df0ff8fc76098d3e
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-12 02:55:01 -0300

    test

--- IRA_Server/incidentes/vistas/dashboard.py
+++ IRA_Server/incidentes/vistas/dashboard.py
@@ -4,14 +4,12 @@ from django.contrib.auth.decorators import login_required
 from ..modelos.dashboard import *
 from django.template import Context
 
+
 @login_required(login_url='login')
 def cargar_dashboard(request):
 
     severidades = DashboardSeveridades()
     topValores = DashboardTop()
-
-    print(topValores.contAmbiente)
-    print(topValores.contServicio)
 
     contexto = {'severidad':severidades,'servicio':topValores.contServicio,'ubicacion':topValores.contUbicacion,
     'ambiente':topValores.contAmbiente,'tipo':topValores.contTipo,'origen':topValores.contOrigen}
