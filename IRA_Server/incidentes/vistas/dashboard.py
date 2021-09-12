d1240873e39a2f024ffc291a6f3cf63134ac1c31
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-12 01:40:28 -0300

    test

--- IRA_Server/incidentes/vistas/dashboard.py
+++ IRA_Server/incidentes/vistas/dashboard.py
@@ -10,6 +10,9 @@ def cargar_dashboard(request):
     severidades = DashboardSeveridades()
     topValores = DashboardTop()
 
+    print(severidades)
+    print(topValores)
+
     contexto = {'severidad':severidades,'servicio':topValores.contServicio,'ubicacion':topValores.contUbicacion,
     'ambiente':topValores.contAmbiente,'tipo':topValores.contTipo,'origen':topValores.contOrigen}
     return render(request, "dashboard.html", contexto)
