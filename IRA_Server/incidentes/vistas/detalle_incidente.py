88d257d29fb43b9f609f497e9c7ecc20b6f1149b
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-12 19:29:15 -0300

    test

--- IRA_Server/incidentes/vistas/detalle_incidente.py
+++ IRA_Server/incidentes/vistas/detalle_incidente.py
@@ -13,18 +13,24 @@ def cargar_incidente(request):
 
         if detalle[0][6] == 1:
             print("No hubo Clientes Afectados")
+            detalle[0][6] = "No hubo Clientes Afectados"
         else:
             print("Hay Clientes Afectados")
+            detalle[0][6] = "Hay Clientes Afectados"
 
         if detalle[0][7] == 1:
             print("No hubo Proveedores Involucrados")
+            detalle[0][7] = "No hubo Proveedores Involucrados"
         else:
             print("Hay Proveedores Involucrados")
+            detalle[0][7] = "Hay Proveedores Involucrados"
 
         if detalle[0][8] == 1:
             print("Activos Afectados 0-25%")
+            detalle[0][8] = "Activos Afectados 0-25%"
         else:
             print("Mayor a 25%")
+            detalle[0][8] = "Mayor a 25%"
     
         contexto = {'detalle':detalle}
         return render(request, "detalle_incidente.html", contexto)
