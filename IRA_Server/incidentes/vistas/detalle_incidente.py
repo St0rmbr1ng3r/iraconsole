3e031a85b47e9501dc48a9b68abc90766c4f2f62
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-25 10:58:12 -0300

    modulo no importado

--- IRA_Server/incidentes/vistas/detalle_incidente.py
+++ IRA_Server/incidentes/vistas/detalle_incidente.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render#, redirect
 #from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from modelos.incidente import *
+from ..modelos.incidente import *
 
 @login_required(login_url='login')
 def cargar_incidente(request):
