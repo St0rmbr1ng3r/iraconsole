9485e0b122ed91fe78b2b0427a7248dc3cc3c6bd
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-25 13:11:18 -0300

    panel de administracion

--- IRA_Server/incidentes/vistas/panel_administracion.py
+++ IRA_Server/incidentes/vistas/panel_administracion.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
 
-@user_passes_test(lambda u: u.is_superuser)
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
 def cargar_admin(request):
-    return render(request, "panel_administracion.html")
+    if request.user.is_superuser:
+        return render(request, "panel_administracion.html")
+
+    return render(request, "dashboard.html")
