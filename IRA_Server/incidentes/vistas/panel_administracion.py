e17826bf303c50eee619a098d0f856a96651ec96
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-25 14:11:15 -0300

    mock listado de usuarios

--- IRA_Server/incidentes/vistas/panel_administracion.py
+++ IRA_Server/incidentes/vistas/panel_administracion.py
@@ -1,10 +1,13 @@
 
+from io import UnsupportedOperation
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def cargar_admin(request):
     if request.user.is_superuser:
-        return render(request, "panel_administracion.html")
+        usuarios = ["username","correo@correo.com","22-09-2021 12:25:25","24-09-2021 16:35:15"]
+        contexto = {usuarios:'usuarios'}
+        return render(request, "panel_administracion.html", contexto)
 
     return redirect('dashboard')
