01dc66587ff9dc237200a1e5d9fe43cd69ed25bd
St0rmbr1ng3r <st0rmbr1ng3r@cyberdude.com>
2021-09-25 14:18:06 -0300

    test

--- IRA_Server/incidentes/vistas/panel_administracion.py
+++ IRA_Server/incidentes/vistas/panel_administracion.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def cargar_admin(request):
     if request.user.is_superuser:
-        usuarios = ["username","correo@correo.com","22-09-2021 12:25:25","24-09-2021 16:35:15"]
+        usuarios = [["username","correo@correo.com","22-09-2021 12:25:25","24-09-2021 16:35:15"],["username2","correo2@correo.com","22-09-2020 12:25:25","24-09-2020 16:35:15"]]
         contexto = {'usuarios':usuarios}
         return render(request, "panel_administracion.html", contexto)
 
