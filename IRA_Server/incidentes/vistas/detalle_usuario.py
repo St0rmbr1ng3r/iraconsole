128b77266d5e6286a3274819ef59db06a19d0621
St0rmbr1ng3r <74727838+St0rmbr1ng3r@users.noreply.github.com>
2021-10-17 19:16:03 -0300

    dsfadf

--- IRA_Server/incidentes/vistas/detalle_usuario.py
+++ IRA_Server/incidentes/vistas/detalle_usuario.py
@@ -7,7 +7,38 @@ from django.contrib import messages
 @login_required(login_url='login')
 def cargar_usuario(request):
     if request.user.is_superuser:
+        if request.method == 'GET':
+            id_usuario=request.GET.get('id_usuario', '0')
+            u = Usuario() #AGREGADO HAY QUE BORRAR
+            detalle = u.cargar_detalle_usuario(id_usuario)
 
+            if detalle == 1:
+                messages.error(request, "Usuario Invalido")
+                #return redirect('usuarioinvalido')
+                return redirect('administracion')
+            '''
+            u.id = int(detalle[0][0])
+            #u.password = detalle[0][1]
+            u.last_login = detalle[0][2]
+            u.is_superuser = int(detalle[0][3])
+            u.username = detalle[0][4]
+            u.first_name = detalle[0][5]
+            u.last_name = detalle[0][6]
+            u.email = detalle[0][7]
+            u.is_active = int(detalle[0][8])
+            u.date_joined = detalle[0][10]
+            '''
+            formulario1 = FormularioModificarUsuario(initial={
+                'id':int(detalle[0][0]), 
+                'is_superuser': int(detalle[0][3]),
+                'username' : detalle[0][4],
+                'first_name' : detalle[0][5],
+                'last_name' : detalle[0][6],
+                'email' : detalle[0][7],
+                'is_active' : int(detalle[0][8]),
+                'date_joined' : detalle[0][10], 
+                'last_login' : detalle[0][2]           
+                })
         #AGREGADO CON METODO POST
         if request.method == 'POST':
             formulario = FormularioModificarUsuario(request.POST, initial=formulario1)
@@ -31,39 +62,8 @@ def cargar_usuario(request):
                 print (formulario.errors)
                 messages.error(request, "No se pueden guardar los cambios. Por favor intente nuevamente" )
                 return redirect('administracion')
-                
-        #if request.method == 'GET':
-        id_usuario=request.GET.get('id_usuario', '0')
-        u = Usuario() #AGREGADO HAY QUE BORRAR
-        detalle = u.cargar_detalle_usuario(id_usuario)
 
-        if detalle == 1:
-            messages.error(request, "Usuario Invalido")
-            #return redirect('usuarioinvalido')
-            return redirect('administracion')
-        '''
-        u.id = int(detalle[0][0])
-        #u.password = detalle[0][1]
-        u.last_login = detalle[0][2]
-        u.is_superuser = int(detalle[0][3])
-        u.username = detalle[0][4]
-        u.first_name = detalle[0][5]
-        u.last_name = detalle[0][6]
-        u.email = detalle[0][7]
-        u.is_active = int(detalle[0][8])
-        u.date_joined = detalle[0][10]
-        '''
-        formulario1 = FormularioModificarUsuario(initial={
-            'id':int(detalle[0][0]), 
-            'is_superuser': int(detalle[0][3]),
-            'username' : detalle[0][4],
-            'first_name' : detalle[0][5],
-            'last_name' : detalle[0][6],
-            'email' : detalle[0][7],
-            'is_active' : int(detalle[0][8]),
-            'date_joined' : detalle[0][10], 
-            'last_login' : detalle[0][2]           
-            })
+        
 
         contexto = {'formulario':formulario1} 
         return render(request, "detalle_usuario.html", contexto)
