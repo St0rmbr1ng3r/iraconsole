c97c98ee621b5d2fb345cff7dbb3bd52d6af4f4f
St0rmbr1ng3r <74727838+St0rmbr1ng3r@users.noreply.github.com>
2021-10-17 18:32:04 -0300

    post

--- IRA_Server/incidentes/vistas/detalle_usuario.py
+++ IRA_Server/incidentes/vistas/detalle_usuario.py
@@ -46,6 +46,7 @@ def cargar_usuario(request):
 
         #AGREGADO CON METODO POST
         if request.method == 'POST':
+            print("Ingreso a METODO POST")
             formulario = FormularioModificarUsuario(request.POST)
 
             print("ID USUARIO: ", formulario['id'])
